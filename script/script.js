843f1141f6c994e0460044e48737e551ffac6f88
Mohamed EL FAQUIR <139956531+infoconsultingtech@users.noreply.github.com>
2023-08-02 16:47:59 +0200

    new projet
    My Website personel

---
+++ script/script.js
@@ -0,0 +1,37 @@
+// add class navbarDark on navbar scroll
+const header = document.querySelector('.navbar');
+
+window.onscroll = function() {
+    var top = window.scrollY;
+    if(top >=100) {
+        header.classList.add('navbarDark');
+    }
+    else {
+        header.classList.remove('navbarDark');
+
+    }
+}
+
+// add annimation paragraphe
+
+const swiftUpElements = document.querySelectorAll('.hero_desc');
+
+swiftUpElements.forEach(elem => {
+
+	const words = elem.textContent.split(' ');
+	elem.innerHTML = '';
+
+	words.forEach((el, index) => {
+		words[index] = `<span><i>${words[index]}</i></span>`;
+	});
+
+	elem.innerHTML = words.join(' ');
+
+	const children = document.querySelectorAll('span > i');
+	children.forEach((node, index) => {
+		node.style.animationDelay = `${index * .2}s`;
+	});
+
+});
+
+
