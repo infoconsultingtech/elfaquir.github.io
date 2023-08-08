0e2a85ba8038349875f0697805ede044772d3e1d
Mohamed EL FAQUIR <139956531+infoconsultingtech@users.noreply.github.com>
2023-08-08 12:24:32 +0200

    ajouter contact form

---
+++ contact.php
@@ -0,0 +1,19 @@
+<?php
+ if(!empty($_POST['surname']) && !empty($_POST['firstname']) && !empty($_POST['email']) && !empty($_POST['message'])){
+
+    $surname = htmlspecialchars($_POST['surname']);
+    $firstname = htmlspecialchars($_POST['firstname']);
+    $email = htmlspecialchars($_POST['email']);
+    $message = htmlspecialchars($_POST['message']);
+
+    if(filter_var($email, FILTER_VALIDATE_EMAIL)){
+
+    }else{
+        echo "email non valide"
+    }
+
+}else{
+header('location:index.html');
+die();
+}
+ 
